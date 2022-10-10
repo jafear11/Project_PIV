ce5db41d7f7ce1278b1ab5fffdf21d33b0bd4393
Javier Fernandez <jafear11@gmail.com>
2022-10-10 22:22:05 +0200

    Algo1 casi esta
    Falta una mica de normalitzacio amb els bins i mirar si es eficient o no

--- Algo1.m
+++ Algo1.m
@@ -1,25 +1,29 @@
-imatges = imageDatastore('Training-Dataset\Images\*.jpg');
-masqueres = imageDatastore('Training-Dataset\Masks-Ideal\*.bmp');
 
-for k= 1:length(imatges)
+%%%%%%%%%%%%%%%% Algorithm 1 %%%%%%%%%%%%%%%%
+imatges = imageDatastore('Training-Dataset\Images\*.jpg');
+mascaras = imageDatastore('Training-Dataset\Masks-Ideal\*.bmp');
+Nbins = 32;
+histograma = zeros(Nbins,Nbins);
+
+
+for k= 1:length(imatges.Files)
     
     %Load the image and the mask
-    image = imread('Training-Dataset\Images\0_A_hgr2B_id01_1.jpg');
-    mask = imread('Training-Dataset\Masks-Ideal\0_A_hgr2B_id01_1.bmp');
+    image = readimage(imatges,k);
+    mask = readimage(mascaras,k);
+    mask_f = mask(:);
 
-    %Flatten them both
+    %Flatten the image
     P = numel(image)/3;
     image_f = reshape(image,P,3);
-    mask_f = mask(:);
+
     %We get an array of only skin pixels
-    final = image_f(~ mask_f); %->This doesnt work
-    %final = rgb2ycbcr(final);
-    histogram(final)    
+    final = image_f(~mask_f, :);
+    final = im2double(final);
+    final = rgb2ycbcr(final);
+    colorbar
+    histograma = histograma + hist3(final(:,2:3),'Nbins',[Nbins,Nbins]); %%%PROBLEMS WITH NORMALIZATION
 
 end
 
-
-function [output] = flatten(input)
-    P = numel(input)/3;
-    output = reshape(input,P,3);
-end
+bar3(histograma)
